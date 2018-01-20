880409eb471ecb94bdec26de3b73a73387c7769e
maialenzab <maialenz95@gmail.com>
2018-01-20 16:51:06 +0100

    Mislikes

--- app/src/main/java/ml/byta/byta/Activities/MisLikes.java
+++ app/src/main/java/ml/byta/byta/Activities/MisLikes.java
@@ -1,9 +1,11 @@
 package ml.byta.byta.Activities;
 
+import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.Point;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutCompat;
@@ -31,10 +33,13 @@ public class MisLikes extends AppCompatActivity {
 
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mis_likes);
+
+        final Activity activity = this;
 
         Display display = getWindowManager().getDefaultDisplay();
         Point size = new Point();
@@ -43,8 +48,8 @@ public class MisLikes extends AppCompatActivity {
         } catch (NoSuchMethodError err) {
             display.getSize(size);
         }
-        int width = size.x;
-        int height = size.y;
+        final int width = size.x;
+        final int height = size.y;
 
 
 
@@ -56,53 +61,71 @@ public class MisLikes extends AppCompatActivity {
          *      Bitmap bitmap = loadImage(object.getServerId());
          */
 
-        List<Object> likedObjects = Database.db.objectDao().getAllViewed();
+        //List<Object> likedObjects = Database.db.objectDao().getAllViewed();
+
+        new AsyncTask<Void, Void, List<Object>>() {
+            @Override
+            protected List<Object> doInBackground(Void... params) {
+                List<Object> likedObjects = Database.db.objectDao().getAllViewed();
+                return likedObjects;
+            }
+
+
+            protected void onPostExecute(List<Object> likedObjects) {
+
+                Log.d("liked", String.valueOf(likedObjects.size()));
+
+
+                FlexboxLayout flex = (FlexboxLayout) findViewById(R.id.likedimages);
+                for(int x=0;x<likedObjects.size();x++) {
+                    //Creamos un LinearLayout
+                    LinearLayout linearLayout = new LinearLayout(activity);
+                    //Y un imageview que irá dentro
+                    ImageView image = new ImageView(MisLikes.this);
+                    image.setImageBitmap(loadImage(likedObjects.get(x).getServerId()));
+
+                    //También otro linearlayout que incluirá los botones
+                    LinearLayout botones = new LinearLayout(MisLikes.this);
+                    //Botones
+                    ImageView superlike = new ImageView(MisLikes.this);
+                    superlike.setImageResource(R.drawable.x_icon);
+
+                    ImageView white = new ImageView(MisLikes.this);
+                    white.setImageResource(R.drawable.white);
+
+                    ImageView dislike = new ImageView(MisLikes.this);
+                    dislike.setImageResource(R.drawable.supericon);
+
+
+                    //Añadimos los botones
+                    botones.addView(superlike,width/9,width/9);
+                    botones.addView(white,width/20, width/20);
+                    botones.addView(dislike,width/9,width/9);
+
+
+                    //Añadimos el imageview al linearLayout
+                    linearLayout.setOrientation(LinearLayout.VERTICAL);
+                    linearLayout.setGravity(Gravity.CENTER);
+                    LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(width/2,height/3);
+
+                    botones.setGravity(Gravity.CENTER);
+                    linearLayout.addView(image);
+                    linearLayout.addView(botones);
+
+                    image.setLayoutParams(lp);
+                    image.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                    //linearLayout.setBackgroundColor(Color.BLUE);
+                    flex.addView(linearLayout);
+
+                }
+
+            }
+        }.execute();
 
 
 
 
 
-        FlexboxLayout flex = (FlexboxLayout) findViewById(R.id.likedimages);
-        for(int x=0;x<9;x++) {
-            //Creamos un LinearLayout
-            LinearLayout linearLayout = new LinearLayout(this);
-            //Y un imageview que irá dentro
-            ImageView image = new ImageView(MisLikes.this);
-            image.setImageResource(R.drawable.byta);
-            //También otro linearlayout que incluirá los botones
-            LinearLayout botones = new LinearLayout(MisLikes.this);
-                //Botones
-                ImageView superlike = new ImageView(MisLikes.this);
-                superlike.setImageResource(R.drawable.x_icon);
-
-                ImageView white = new ImageView(MisLikes.this);
-                white.setImageResource(R.drawable.white);
-
-                ImageView dislike = new ImageView(MisLikes.this);
-                dislike.setImageResource(R.drawable.supericon);
-
-
-                //Añadimos los botones
-                botones.addView(superlike,width/9,width/9);
-                botones.addView(white,width/20, width/20);
-                botones.addView(dislike,width/9,width/9);
-
-
-            //Añadimos el imageview al linearLayout
-            linearLayout.setOrientation(LinearLayout.VERTICAL);
-            linearLayout.setGravity(Gravity.CENTER);
-            LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(width/2,height/3);
-
-            botones.setGravity(Gravity.CENTER);
-            linearLayout.addView(image);
-            linearLayout.addView(botones);
-
-            image.setLayoutParams(lp);
-            image.setScaleType(ImageView.ScaleType.CENTER_CROP);
-            //linearLayout.setBackgroundColor(Color.BLUE);
-            flex.addView(linearLayout);
-
-        }
 
     }
 
