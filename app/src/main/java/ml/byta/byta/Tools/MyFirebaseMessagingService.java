0ca32c48a21cc408870d0ea2a9a2b10902cc3fc8
maialenzab <maialenz95@gmail.com>
2018-01-24 16:41:06 +0100

    Notificacion interna de nuevos mensajes en el chat

--- app/src/main/java/ml/byta/byta/Tools/MyFirebaseMessagingService.java
+++ app/src/main/java/ml/byta/byta/Tools/MyFirebaseMessagingService.java
@@ -1,6 +1,8 @@
 package ml.byta.byta.Tools;
 
 
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -15,14 +17,25 @@ public class MyFirebaseMessagingService  extends FirebaseMessagingService {
 
     private static final String TAG = "FCM Service";
     @Override
-    public void onMessageReceived(RemoteMessage remoteMessage) {
+    public void onMessageReceived(final RemoteMessage remoteMessage) {
         // TODO: Handle FCM messages here.
         // If the application is in the foreground handle both data and notification messages here.
         // Also if you intend on generating your own notifications as a result of a received FCM
         // message, here is where that should be initiated.
-        //Log.d(TAG, "From: " + remoteMessage.getFrom());
-        //Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
-        //Log.d("entro","notificacion");
-        Toast.makeText(this, "NOTIFICACION", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+        Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
+        Log.d(TAG, "Notification Message Body: " + remoteMessage.getData());
+        Log.d("entro","notificacion");
+
+        Handler handler = new Handler(Looper.getMainLooper());
+        handler.post(new Runnable() {
+            public void run() {
+                Toast.makeText(getApplicationContext(), "Tienes nuevos mensajes", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
     }
+
 }
